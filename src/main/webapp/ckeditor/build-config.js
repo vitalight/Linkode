f09f2e2ed3199e428f10aa75dfd7f7bdb56e709b
jason <jasonliu747@gmail.com>
2017-07-08 18:02:06 +0800

    文本编辑器功能实现

--- src/main/webapp/ckeditor/build-config.js
+++ src/main/webapp/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/fe0eeec95a879fdafd8ad40e189c1bfe
+ * (2) http://ckeditor.com/builder/7ee74a76007e67dbff40a794752eaafa
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/fe0eeec95a879fdafd8ad40e189c1bfe
+ * (3) http://ckeditor.com/builder/download/7ee74a76007e67dbff40a794752eaafa
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -53,6 +53,7 @@ var CKBUILDER_CONFIG = {
 	plugins : {
 		'a11yhelp' : 1,
 		'about' : 1,
+		'autocorrect' : 1,
 		'autoembed' : 1,
 		'basicstyles' : 1,
 		'blockquote' : 1,
@@ -65,6 +66,7 @@ var CKBUILDER_CONFIG = {
 		'entities' : 1,
 		'filebrowser' : 1,
 		'floatingspace' : 1,
+		'font' : 1,
 		'format' : 1,
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
--- src/main/webapp/ckeditor/config.js
+++ src/main/webapp/ckeditor/config.js
@@ -9,30 +9,77 @@ CKEDITOR.editorConfig = function( config ) {
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
+    config.toolbar = [
+        { name: 'clipboard', items: [ 'Undo', 'Redo' ] },
+        { name: 'styles', items: [ 'Font', 'FontSize' ] },
+        { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Strike', 'Underline', '-', 'RemoveFormat' ] },
+        { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ] },
+        { name: 'links', items: [ 'Link', 'Unlink' ] },
+        { name: 'insert', items: [ 'Image', 'EmbedSemantic', 'Table' ] },
+        { name: 'editing', items: [ 'AutoCorrect' ] },
+        { name: 'mode', items: [ 'Source' ] }
+        //{ name: 'tools', items: [ 'Maximize' ] },
+    ];
+
+    config.contentsCss = [ 'https://cdn.ckeditor.com/4.7.1/standard-all/contents.css', 'mystyle.css'];
+    config.fontawesomePath = '././font-awesome/css/font-awesome.min.css';
+    config.font_names = '宋体/宋体;黑体/黑体;仿宋/仿宋_GB2312;楷体/楷体_GB2312;隶书/隶书;幼圆/幼圆;微软雅黑/微软雅黑;'+ config.font_names;
+    config.allowedContent = true;
+    config.language = 'zh-cn';
+    config.removePlugins = 'image';
+    config.height = 461;
+    config.bodyClass = 'article-editor';
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	//config.removeButtons = 'Underline,Subscript,Superscript';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	config.stylesSet = [
+        /* Inline Styles */
+        { name: 'Marker',			element: 'span', attributes: { 'class': 'marker' } },
+        { name: 'Cited Work',		element: 'cite' },
+        { name: 'Inline Quotation',	element: 'q' },
+
+        /* Object Styles */
+        {
+            name: 'Special Container',
+            element: 'div',
+            styles: {
+                padding: '5px 10px',
+                background: '#eee',
+                border: '1px solid #ccc'
+            }
+        },
+        {
+            name: 'Compact table',
+            element: 'table',
+            attributes: {
+                cellpadding: '5',
+                cellspacing: '0',
+                border: '1',
+                bordercolor: '#ccc'
+            },
+            styles: {
+                'border-collapse': 'collapse'
+            }
+        },
+        { name: 'Borderless Table',		element: 'table',	styles: { 'border-style': 'hidden', 'background-color': '#E6E6FA' } },
+        { name: 'Square Bulleted List',	element: 'ul',		styles: { 'list-style-type': 'square' } },
+
+        /* Widget Styles */
+        // We use this one to style the brownie picture.
+        { name: 'Illustration', type: 'widget', widget: 'image', attributes: { 'class': 'image-illustration' } },
+        // Media embed
+        { name: '240p', type: 'widget', widget: 'embedSemantic', attributes: { 'class': 'embed-240p' } },
+        { name: '360p', type: 'widget', widget: 'embedSemantic', attributes: { 'class': 'embed-360p' } },
+        { name: '480p', type: 'widget', widget: 'embedSemantic', attributes: { 'class': 'embed-480p' } },
+        { name: '720p', type: 'widget', widget: 'embedSemantic', attributes: { 'class': 'embed-720p' } },
+        { name: '1080p', type: 'widget', widget: 'embedSemantic', attributes: { 'class': 'embed-1080p' } }
+    ]
 };
